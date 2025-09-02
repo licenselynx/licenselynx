1db3ec83953258025b307619377818b1e605e0da
Leo Reinmann <leo.reinmann@siemens.com>
2025-09-02 15:52:14 +0200

    refactor(java): update assertions in LicenseLynxTest for clarity

--- java/src/test/java/org/licenselynx/LicenseLynxTest.java
+++ java/src/test/java/org/licenselynx/LicenseLynxTest.java
@@ -57,8 +57,8 @@ class LicenseLynxTest
 
         // Assert
         assert result != null;
-        Assertions.assertEquals(result.getCanonical(), "testCanonical");
-        Assertions.assertEquals(result.getSrc(), "testSrc");
+        Assertions.assertEquals("testCanonical", result.getCanonical());
+        Assertions.assertEquals("testSrc", result.getSrc());
     }
 
     @Test
@@ -88,8 +88,8 @@ class LicenseLynxTest
 
         // Assert
         assert result != null;
-        Assertions.assertEquals(result.getCanonical(), "testCanonical");
-        Assertions.assertEquals(result.getSrc(), "testSrc");
+        Assertions.assertEquals("testCanonical", result.getCanonical());
+        Assertions.assertEquals("testSrc", result.getSrc());
     }
 
 
